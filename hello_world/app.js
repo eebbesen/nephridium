e27aac47d2f8fcddc1a507a89ff2a1ea4cc85a33
eebbesen <eebbesen.git@gmail.com>
2018-10-05 15:11:00 -0500

    works locally and via cloudfront

--- hello_world/app.js
+++ hello_world/app.js
@@ -18,7 +18,7 @@ let response;
  * @param {Object} event.body - A JSON string of the request payload.
  * @param {boolean} event.body.isBase64Encoded - A boolean flag to indicate if the applicable request payload is Base64-encode
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
  * @param {Object} context
  * @param {string} context.logGroupName - Cloudwatch Log Group name
  * @param {string} context.logStreamName - Cloudwatch Log stream name.
@@ -35,22 +35,28 @@ let response;
  * @returns {string} object.statusCode - HTTP Status Code to be returned to the client
  * @returns {Object} object.headers - HTTP Headers to be returned
  * @returns {Object} object.body - JSON Payload to be returned
- * 
+ *
  */
 exports.lambdaHandler = async (event, context) => {
-    try {
-        const ret = await axios(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-                location: ret.data.trim()
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
+  try {
+    let params = null;
+    console.log('EVENT: ', event);
+    console.log('EVENT_QSP: ', event.queryStringParameters);
+    if (event && event.queryStringParameters){
+      params = event.queryStringParameters;
     }
+    const ret = await axios(url);
+    response = {
+      'statusCode': 200,
+      'body': JSON.stringify({
+        message: 'hello worldsss' + JSON.stringify(params),
+        location: ret.data.trim()
+      })
+    }
+  } catch (err) {
+      console.log(err);
+      return err;
+  }
 
-    return response
+  return response
 };
