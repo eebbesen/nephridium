d7d64442276710cf712a8e135a144ba0bf890b73
J2EEbbesen <eebbesen@users.noreply.github.com>
2019-07-27 13:14:46 -0500

    UI updates (#40)
    * incrementing version
    
    * adding package version
    
    * button UI
    
    * linting

--- nephridium/app.js
+++ nephridium/app.js
@@ -4,6 +4,7 @@ const tableify = require('tableify');
 // const dayMs = 86400000;
 const weekMs = 604800000;
 const thirtyDayMs = 2592000000;
+const releaseVersion = require('./package.json').version;
 
 /**
  *
@@ -144,12 +145,11 @@ exports.html = function (data, socrataUrl) {
     </h1>
   </div>
   <div>
-    <button id="download" type="button" onclick="exportTableToCSV('data.csv')">Download this data for Excel</button>
+    <button id="downloadCSV" type="button" onclick="exportTableToCSV('data.csv')">Download this data for a spreadsheet</button>
+    <button id="downloadJSON" type="button" onclick="location.href='${socrataUrl}'">Raw JSON from Socrata</button>
   </div>
   <div>${display}</div>
-  <div>
-    <button type="button" onclick="location.href='${socrataUrl}'">Raw JSON from Socrata</button>
-  </div>
+  <div id="version">nephridium version: ${releaseVersion}</div>
 
   ${this.javascript()}
 </body>
@@ -235,6 +235,22 @@ td {
 h1 {
   margin: 0;
 }
+
+button {
+  border: 2px solid blue;
+  border-radius: 4px;
+}
+
+button:hover {
+  color: white;
+  cursor: pointer;
+  background-color: blue;
+}
+
+#version {
+  text-align: center;
+  font-size: 1em;
+}
   `;
 };
 
@@ -274,7 +290,7 @@ exports.javascript = function () {
 };
 
 // expects lat/long only
-exports.mapIt = function(address) {
+exports.mapIt = function (address) {
   const URL = 'https://www.google.com/maps/place/';
   const POST = '%20Saint+Paul,+MN';
   return `<a href="${URL + encodeURIComponent(address) + POST}">${address}</a>`;
