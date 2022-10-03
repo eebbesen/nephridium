9d01cc46c228cf80bed81282233bba07632180d1
eebbesen <ebbesen.git@gmail.com>
2022-10-02 20:10:57 -0500

    refactoring

--- nephridium/tests/unit/test_data_utils.js
+++ nephridium/tests/unit/test_data_utils.js
@@ -1,20 +1,69 @@
 const chai = require('chai');
-const data_utils = require('../../data_utils.js');
+const dataUtils = require('../../data_utils.js');
 
 const { expect } = chai;
 
 describe('transformDate', () => {
   it('transforms esri date to ISO string', () => {
-    const ret = data_utils.transformDate(1546560000000);
+    const ret = dataUtils.transformDate(1546560000000);
     expect(ret.getTime()).to.equal(new Date('2019-01-04').getTime());
   });
 
   it('returns null given null', () => {
-    expect(data_utils.transformDate(null)).to.be.null;
+    expect(dataUtils.transformDate(null)).to.be.null;
   });
 
   it('transforms UTC ISO string to date', () => {
-    const ret = data_utils.transformDate("2022-08-30T00:00:00.000Z");
+    const ret = dataUtils.transformDate("2022-08-30T00:00:00.000Z");
     expect(ret.getTime()).to.equal(1661817600000);
   });
 });
+
+describe('transformData', () => {
+  it('simplifies dates without timestamp', () => {
+    const data = [{
+      name_one: '2018-10-01T00:00:00.000',
+      a: '2018-10-01T00:00:01.000',
+      b: 2,
+      c_you_later: '3',
+      d: '7T989',
+    }];
+
+    const result = dataUtils.transformData(data)[0];
+    expect(result['name_one']).to.equal('2018-10-01');
+    expect(result.a).to.equal('2018-10-01 00:00:01');
+    expect(result.b).to.equal(2);
+    expect(result['c_you_later']).to.equal('3');
+    expect(result.d).to.equal('7T989');
+  });
+
+  it('decorates locations', () => {
+    const data = [{
+      name_one: '2018-10-01T00:00:00.000',
+      location: '1600 Grand Ave',
+    }];
+    const result = dataUtils.transformData(data)[0];
+    expect(result.location).to.equal('<a href="https://www.google.com/maps/place/1600%20Grand%20Ave%20Saint+Paul,+MN">1600 Grand Ave</a>');
+  });
+
+  it('changes timestamp into date', () => {
+    const data = [{
+      name_one: '2018-10-01T00:00:00.000',
+      a: '2018-10-01T00:00:01.000',
+      b: 2,
+      c_you_later: '3',
+      d: '7T989',
+      e: 1663891200000
+    }];
+
+    const result = dataUtils.transformData(data)[0];
+    expect(result.e).to.equal(new Date(1663891200000).toISOString().slice(0,10));
+  });
+});
+
+describe('mapIt', () => {
+  const result = dataUtils.mapIt('1600 Grand Ave');
+
+  expect(result).to.equal('<a href="https://www.google.com/maps/place/1600%20Grand%20Ave%20Saint+Paul,+MN">1600 Grand Ave</a>');
+});
+
--- nephridium/ui_utils.js
+++ nephridium/ui_utils.js
@@ -1,3 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
+const releaseVersion = require('./package.json').version;
+
 const paramsToRemove = ['time_column', 'url', 'time_range', 'to_remove', 'display_title', 'provider'];
 
 
@@ -19,4 +24,89 @@ exports.stringifyParams = function (params) {
   });
 
   return pString;
+};
+
+exports.css = function () {
+  return fs.readFileSync(path.resolve(__dirname, './assets/nephridium.css'), 'utf8');
+};
+
+exports.javascript = function () {
+  return Object.freeze(`
+  <script type="text/javascript">
+    ${fs.readFileSync(path.resolve(__dirname, './assets/nephridium.js'), 'utf8')}
+  </script>`);
+};
+
+exports.buildTableData = function (data) {
+  if (data == null || data.length < 1) {
+    return '<div class="error"><p>No records found</p><p>Please expand your search</p></div>';
+  }
+
+  const keys = Object.keys(data[0]);
+  const tableHead = keys.map(k => `<th>${k.replace(/_/g," ")}</th>`).join('');
+  const tableData = keys.map(k => `<td>\${${k}}</td>`).join('');
+  const bodyDataTemplate = { '<>': 'tr', html: tableData };
+
+  return `<div id="data_table"><table><thead><tr>${tableHead}</tr></thead><tbody>${json2html.transform(data, bodyDataTemplate)}</tbody></table></div>`;
+};
+
+exports.buildFiltersDisplay = function (params) {
+  let filter = '';
+  if (params) {
+    delete params.display_title;
+
+    const fs = Object.keys(params).map(k => `<li>${k.toUpperCase().replace(/_/g, ' ')}: ${(params[k]).toString().toLowerCase()}</li>`);
+    let fss = '';
+    fs.forEach(f => fss += f);
+    filter = `
+<div id="filters" style="display:none">
+  <h2>Filters</h2>
+  <ul>
+    ${fss}
+  </ul>
+</div>
+`;
+  }
+
+  return filter;
+};
+
+exports.html = function (data, dataUrl, params, datasetUrl) {
+  return Object.freeze(`
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+  <style>${this.css()}</style>
+  <title>Nephridium-powered page</title>
+  <link rel="shortcut icon" href="#" />
+  <link rel="shortcut icon" type="image/png" href="https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Filter_font_awesome.svg/32px-Filter_font_awesome.svg.png"/>
+</head>
+<body>
+  <div id="description">
+    <h1>
+      <a href="${datasetUrl}">${this.getDisplayTitle(params)}</a>
+    </h1>
+  </div>
+
+  <div id="buttons">
+    <button id="downloadCSV" type="button" onclick="exportTableToCSV('data.csv')">Download this data for a spreadsheet</button>
+    <button id="downloadJSON" type="button" onclick="location.href='${dataUrl}'">Raw JSON</button>
+    <button id="toggleFilters" type="button" onclick="toggleFilterDisplay()">Show Filters</button>
+  </div>
+  ${this.buildFiltersDisplay(params)}
+  <div>${this.buildTableData(data)}</div>
+  <div id="footer">
+    <div class="footerElement" id="footerElementLeft">nephridium version: ${releaseVersion}</div><div id="footerElementRight" class="footerElement">Get the source code <a id="github" class="footerElement" href="https://github.com/eebbesen/nephridium">here</a></div>
+  </div>
+  ${this.javascript()}
+</body>
+</html>`);
+};
+
+exports.getDisplayTitle = function (params) {
+  if (params && params.display_title && params.display_title.length > 0) {
+    return params.display_title;
+  }
+
+  return '';
 };
